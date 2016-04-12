9ce50c324bbc97e1d0cbdf6a745d13c7d84256b4
Anupam Singh <anupamsngh518@gmail.com>
2016-04-12 14:56:56 +0530

    Added files via upload

---
+++ BootstrapTemplate/Views/footer.php
@@ -0,0 +1,9 @@
+<footer>
+            <div class="row bg-black">
+                <div class="col-md-16" style="text-align:center">
+                    <br>
+                     &copy; 2016 AN OPENSOURCE PROJECT
+                    <br>
+                </div>
+            </div>
+</footer>
---
+++ BootstrapTemplate/Views/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset=utf-8>
+        <meta http-equiv=X-UA-Compatible content="IE=edge">
+        <meta name=viewport content="width=device-width,initial-scale=1">
+        <meta name=keywords content="HTML, CSS, JS, JavaScript, framework, bootstrap, front-end, frontend, web development"> 
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title> Customize and download &middot; Bootstrap </title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <link href=../bootstrap-3.3.6-dist/css/bootstrap.min.css rel=stylesheet>
+        <script src=../bootstrap-3.3.6-dist/js/bootstrap.js></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php include '../Views/header.php'; ?>
+
+
+
+
+
+            <div class="row">
+                <div class="col-sm-12 bg-custom">
+                    <h1>Bootstrap Template</h1>
+                    <p>A one stop spot to give you a detailed study of all the components and styles available in bootstrap.</p>
+                </div>
+            </div>
+          <!-- Main component for a primary marketing message or call to action -->
+
+
+
+        <?php include '../Views/footer.php'; ?>
+     </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="../../dist/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
